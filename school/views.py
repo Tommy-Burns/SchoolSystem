bcda3ce537c0cf9987247674b891c3a8bbfae4d8
Tommy-Burns <58440453+Tommy-Burns@users.noreply.github.com>
2022-06-30 08:39:07 +0000

    up to search

--- school/views.py
+++ school/views.py
@@ -4,9 +4,18 @@ from .models import Courses
 
 # Create your views here.
 def index(request):
-    courses = Courses.objects.all()
-    return render(request, 'index.html', {
-        'courses': courses,
-    })
+    searchTerm = request.GET.get('searchCourse')
+    null_text = "We couldn't find a course that matches your seach criteria."
+    if searchTerm:
+        courses = Courses.objects.filter(name__icontains=searchTerm)       
+    else:
+        courses = Courses.objects.all()
+    if len(courses) == 0:
+        return render(request, 'index.html', {'null': null_text})
+    else:
+        return render(request, 'index.html', {
+            'courses': courses,
+            'searchTerm': searchTerm,
+        })
 
 
