20b1e37eb20db87be4ed2e1b5b6958ff9f65d2e4
Tommy-Burns <58440453+Tommy-Burns@users.noreply.github.com>
2022-07-07 09:13:40 +0000

    search improved. enrolled and not enrolled distinguished and whether or not a user is logged in or not

--- school/views.py
+++ school/views.py
@@ -11,35 +11,58 @@ from accounts.models import RegisteredCourses
 def index(request):
     searchTerm = request.GET.get('searchCourse')
     null_text = "We couldn't find a course that matches your seach criteria."
+    
+    if searchTerm:
+        
+        courses = Courses.objects.all()
+        searched_courses = Courses.objects.filter(name__icontains=searchTerm)       
+
+        if len(searched_courses) == 0:
+            return render(request, 'index.html', {
+                'null': null_text,
+                'searchTerm': searchTerm,
+                })
+        else:
+            if request.user:        
+                username = request.user.username
+                user_courses = RegisteredCourses.objects.filter(user_name=username)
+                enroll_excluded = searched_courses.filter(name__in=[i.registered_course for i in user_courses])
+                found_courses = searched_courses.difference(enroll_excluded)
+                
+                if len(found_courses) == 0:
+                    return render(request, 'index.html', {
+                    'null': null_text + " If a course is already enrolled, it wouldn't appear here.",
+                    'searchTerm': searchTerm,
+                    })
+                else:
+                    return render(request, 'index.html', {
+                        'found_courses': found_courses,
+                        'searchTerm': searchTerm,
+                    })
+            else:
+                return render(request, 'index.html', {
+            'courses': searched_courses,
+            })
     courses = Courses.objects.all()
-    if request.user:
+    
+    if request.user:        
         username = request.user.username
         user_courses = RegisteredCourses.objects.filter(user_name=username)
         enroll_excluded = Courses.objects.filter(name__in=[i.registered_course for i in user_courses])
         not_enrolled = courses.difference(enroll_excluded)
 
-        print('***********************COURSES********************', courses)
-        print('***********************EXCLUDED COURSES********************', enroll_excluded)
-        print('**********************NOT ENROLLED*********************', not_enrolled)
+        # print('***********************COURSES********************', courses)
+        # print('***********************EXCLUDED COURSES********************', enroll_excluded)
+        # print('**********************NOT ENROLLED*********************', not_enrolled)
         return render(request, 'index.html', {
             'courses': courses,
             'enroll_excluded': enroll_excluded,
             'not_enrolled': not_enrolled,
         })
         
-    if searchTerm:
-        courses = Courses.objects.filter(name__icontains=searchTerm)       
-    
-    if len(courses) == 0:
-        return render(request, 'index.html', {
-            'null': null_text,
-            'searchTerm': searchTerm,
-            })
     else:
         return render(request, 'index.html', {
             'courses': courses,
-            'searchTerm': searchTerm,
-            # 'reg_courses': user_reg_courses,
         })
 
 
